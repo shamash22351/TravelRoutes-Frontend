0d8d4cf65c0bec00d085968017eea16015a41e0b
shamash22351 <103871390+shamash22351@users.noreply.github.com>
2025-03-01 00:16:12 +0300

    Update UserContext.js

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -2,14 +2,28 @@ import React, { createContext, useContext, useState } from 'react';
 
 const UserContext = createContext();
 
-export const UserProvider = ({ children }) => {
+const UserProvider = ({ children }) => {
     const [userId, setUserId] = useState(null);
+    const [update, setUpdate] = useState(false); // Добавлено состояние для обновления
+
+    const changeUserId = (id) => {
+        setUserId(id);
+        setUpdate(prev => !prev); // Переключаем состояние для обновления
+    };
 
     return (
-        <UserContext.Provider value={{ userId, setUserId }}>
+        <UserContext.Provider value={{ userId, setUserId: changeUserId }}>
             {children}
         </UserContext.Provider>
     );
 };
 
-export const useUser = () => useContext(UserContext);
+const useUser = () => {
+    const context = useContext(UserContext);
+    if (!context) {
+        throw new Error('useUser must be used within a UserProvider');
+    }
+    return context;
+};
+
+export { UserProvider, useUser };
